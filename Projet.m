7a0c0b498f5192881a11aa39c861e11dcb7c3a99
ALDASORO Louis <Louis.Aldasoro@bordeaux-inp.fr>
2020-12-11 16:34:31 +0100

    Mis à jour préli

--- Projet.m
+++ Projet.m
@@ -8,73 +8,65 @@ clc;
 %Paramètres
 N = 5000;
 p = 4;
-s = 0.5;
 f = -0.5:1/N:0.5-1/N;
 
-prephi = randn(p,1);
-phi = prephi./sum(abs(prephi));
 
-x = zeros(N,1);
+pole = abs(randn(p,1));
+pole = pole/sum(pole);
 
-bbg = s*randn(N,1);
-for i = 1:N
-    for k = 1:p
-        if i-k > 0
-            x(i) = x(i) + phi(k)*x(i-k);
-        end
-    end
-    x(i) = x(i) + bbg(i);
-end
+pole = [1;pole];
+e = randn(N,1);
+x = filter(1, pole, e);
+  
+H = freqz(1,pole,2*pi*f);
 
-X = fftshift(fft(x));
-DSP_x = abs(X).^2;
+X = fftshift(fft(x)).^2/N;
+DSP_x = abs(H).^2;
 
 figure
-subplot 311
+subplot 211
 plot(x); 
 title("Signal autorégressif d'ordre 4");
 ylabel('Amplitude');
 
-subplot 312
+
+subplot 212
 plot(f,abs(X));
-title("Spectre de puissance");
+hold on;
+plot(f,DSP_x,'LineWidth',2);
+title("Spectre et densité spectrale de puissance");
 xlabel('Fréquence normalisée');
 ylabel('Amplitude');
 
-subplot 313
-plot(f,DSP_x);
-title("Densité spectrale de puissance");
-xlabel('Fréquence normalisée');
-ylabel('Amplitude');
+
 
 %% Préliminaire 2
 mu = 0;
-RSB = -5; %rapport signal sur bruit
+RSB = [-5,0,10]; %rapport signal sur bruit
 Ps = sum(abs(X))/N; %puissance signal x(t)
-sig = sqrt(Ps * 10^(-RSB/10)); %puissance bruit sqrt
+R = randn(N,1);
 
-noise = mu + sig*randn(N,1);
+for i=1:3
+    sig(i) = sqrt(Ps * 10^(-RSB(i)/10));
+    noise(i) = mu + sig(i)*R;
+
 y = x + noise; %signal bruité
 
-Y = fftshift(fft(y));
-DSP_y = abs(Y).^2;
+Y = fftshift(fft(y)).^2/N;
+DSP_y = abs(H).^2;
 
 figure
-subplot 311
+subplot 211
 plot(y); 
 title("Représentation temporelle du signal bruité");
 ylabel('Amplitude');
 
-subplot 312
-plot(f,abs(X));
-title("Spectre de puissance");
+subplot 212
+plot(f,abs(Y));
+hold on;
+plot(f,DSP_y,'LineWidth',2);
+title("Spectre et densité spectrale de puissance");
 xlabel('Fréquence normalisée');
 ylabel('Amplitude');
 
-subplot 313
-plot(f,DSP_y);
-title("Densité spectrale de puissance");
-xlabel('Fréquence normalisée');
-ylabel('Amplitude');
-
-
+end
